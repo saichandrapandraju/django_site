83caada49e4866d471e27d73a97828321212eeeb
saichandrapandraju <saichandrapandraju@gmail.com>
2020-07-02 00:21:22 +0530

    added augmented_cnn model & alignment fixes for projects.html

--- gender_recog/models.py
+++ gender_recog/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Check(models.Model):
-    name = models.CharField(max_length=20,unique=True)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class CheckF(models.Model):
-    for_name = models.ForeignKey(Check,on_delete=models.CASCADE)
+    for_name = models.ForeignKey(Check, on_delete=models.CASCADE)
     url = models.URLField(max_length=30)
 
     def __str__(self):
         return self.url
 
+
 class Image_data(models.Model):
-    image = models.FileField(upload_to='uploaded_images/')
+    image = models.FileField(upload_to='gender_recog/uploaded_images/')
--- static/projects.js
+++ static/projects.js
@@ -6,7 +6,7 @@ new fullpage("#fullpage", {
     const title = section.querySelector("h1");
     const tl = new TimelineMax({ delay: 0.5 });
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: 0, opacity: 1 });
-    if (destination.index === 0) {
+    if (destination.index === 1) {
       const face = document.querySelectorAll(".face");
       const description = document.querySelector(".description");
 
